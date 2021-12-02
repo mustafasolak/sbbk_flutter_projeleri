1c403680516491421f0e4b63774afa0de1dc0a61
Mustafa SOLAK <mustafasolak22@hotmail.com>
2021-12-02 18:57:35 +0300

    Column widgeti
    Column widgeti ve özellikleri çalışıldı. Hala eksikler var. Haftaya devam edilecek.

--- sbbk_proje_1/lib/cok_cocuklu_widgetlar.dart
+++ sbbk_proje_1/lib/cok_cocuklu_widgetlar.dart
@@ -2,57 +2,38 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Column(
-      children: [
-        Text(
-          "Ben metin 1",
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            color: Colors.yellow,
-            fontSize: 25,
-          ),
-        ),
-        SizedBox(
-          height: 30.0,
-        ),
-        Text(
-          "Ben metin 2",
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            color: Colors.green,
-            fontSize: 25,
-          ),
-        ),
-        SizedBox(
-          height: 50.0,
-        ),
-        Text(
-          "Ben metin 3",
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            color: Colors.purple,
-            fontSize: 25,
-          ),
-        ),
-        SizedBox(
-          height: 50.0,
-        ),
-        Row(
-          textDirection: TextDirection.ltr,
-          children: [
-            Text(
-              "Metin 1",
-              textDirection: TextDirection.ltr,
+    Center(
+      child: Column(
+        textDirection: TextDirection.ltr,
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Ben Kazım",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.blue,
             ),
-            SizedBox(width: 30,),
-            Text(
-              "Metin 2",
-              textDirection: TextDirection.ltr,
+          ),
+          Text(
+            "Ben Mahsun",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.purple,
             ),
-
-          ],
-        )
-      ],
+          ),
+          Text(
+            "Selam size",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.orangeAccent,
+            ),
+          ),
+        ],
+      ),
     ),
   );
 }
