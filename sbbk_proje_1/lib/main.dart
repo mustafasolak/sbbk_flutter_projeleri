8a8b671b062f60b20864fc674ab5d16bd64369a6
Mustafa SOLAK <mustafasolak22@hotmail.com>
2022-02-17 18:46:26 +0300

    Kullanıcı Giriş Ekranı
    Kullanıcı Giriş Ekranı uygulamasına başlandı.

--- sbbk_proje_1/lib/main.dart
+++ sbbk_proje_1/lib/main.dart
@@ -1,17 +1,53 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    Text(
+  TextEditingController controllerEmail = new TextEditingController();
+  TextEditingController controllerPassword = new TextEditingController();
 
-      "İlk Uygulama Projemiz - 12/G ŞBBK",
-      textDirection: TextDirection.ltr,
-      style: TextStyle(
-        color: Colors.green,
-        fontWeight: FontWeight.bold,
-        fontSize: 30.0,
-      ),
-
-    ),
-  );
+  runApp(MaterialApp(
+    home: Scaffold(
+        appBar: AppBar(
+          title: Text("Kullanıcı Giriş Ekranı"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text("Kullanıcı Girişi"),
+              SizedBox(
+                height: 15,
+              ),
+              TextField(
+                controller: controllerEmail,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: "Kullanıcı adınızı giriniz",
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextField(
+                controller: controllerPassword,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(hintText: "Parolanızı giriniz"),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  String email = controllerEmail.text;
+                  String password = controllerPassword.text;
+                  print("Email :$email, password=$password");
+                  if (email == "mustafa@gmail.com" && password == "123") {
+                    // Ana sayfaya yönlendirilecek
+                  }
+                },
+                child: Text("Giriş Yap"),
+              )
+            ],
+          ),
+        )),
+  ));
 }
